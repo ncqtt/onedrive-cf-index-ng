cab7ee829e4a30b7c81b97a6855ea3738559d6f5
Nguyen Cao An Tuong <anntuongg@gmail.com>
2026-04-06 20:29:04 +0700

    Refactor Redis integration for token storage

--- src/utils/odAuthTokenStore.ts
+++ src/utils/odAuthTokenStore.ts
@@ -1,34 +1,17 @@
-import Redis from 'ioredis'
+import { Redis } from '@upstash/redis'
 
-const redis = process.env.REDIS_URL ? new Redis(process.env.REDIS_URL) : undefined
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL || '',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || '',
+})
 
-export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
-  if (!redis) {
-    throw new Error('REDIS_URL is not defined in environment variables')
-  }
-
+export async function getOdAuthTokens() {
   const accessToken = await redis.get('access_token')
   const refreshToken = await redis.get('refresh_token')
-
-  return {
-    accessToken,
-    refreshToken,
-  }
+  return { accessToken, refreshToken }
 }
 
-export async function storeOdAuthTokens({
-  accessToken,
-  accessTokenExpiry,
-  refreshToken,
-}: {
-  accessToken: string
-  accessTokenExpiry: number
-  refreshToken: string
-}): Promise<void> {
-  if (!redis) {
-    throw new Error('REDIS_URL is not defined in environment variables')
-  }
-
-  await redis.set('access_token', accessToken, 'EX', accessTokenExpiry)
+export async function storeOdAuthTokens({ accessToken, accessTokenExpiry, refreshToken }: any) {
+  await redis.set('access_token', accessToken, { ex: accessTokenExpiry })
   await redis.set('refresh_token', refreshToken)
 }
